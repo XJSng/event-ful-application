a179db5742e133200186800f9ab73979247511f5
Xu Jie Sng <sngxujie@gmail.com>
2024-02-07 06:34:37 +0000

    CRD of event table

--- index.js
+++ index.js
@@ -133,6 +133,68 @@ async function main() {
         })
     })
 
+    // CREATE event route
+    app.get("/event/create", async (req, res)=>{
+        const [event] = await connection.execute(`
+        SELECT * FROM event`)
+        const [organiser] = await connection.execute(`SELECT * FROM organiser`)
+        const [participant] = await connection.execute(`SELECT * FROM participant`)
+        res.render("event/create", {
+            event, organiser, participant
+        })
+    })
+
+
+    app.post("/event/create", async(req, res)=>{
+        const title = req.body.title
+        // change date from form to database format
+        const unformatedDateString = `${req.body.date} ${req.body.time}`
+        const joinedDateTime = new Date(unformatedDateString)
+        const formattedDate = formatDate(joinedDateTime)
+        const date_time = formattedDate
+        const location = req.body.location
+        const participant_id = req.body.participant_id
+        const organiser_id = req.body.organiser_id
+        const query = `INSERT INTO event (title, date_time, location, participant_id, organiser_id)
+        VALUES (?, ?, ?, ?, ?)`
+        const bindings = [title, date_time, location, participant_id, organiser_id]
+        const [result] = await connection.execute(query, bindings)
+        res.redirect('/event')
+    })
+
+    // UPDATE
+    app.get("/event/:event_id/update", async(req, res)=>{
+        let query = `SELECT event.* , organiser.name, organiser.contact_number, organiser.email FROM event JOIN organiser
+        ON event.organiser_id = organiser.organiser_id
+        JOIN participant ON event.participant_id = participant.participant_id WHERE event_id`
+        let organiserQuery = `SELECT * FROM organiser`
+        const [events] = await connection.execute(query, [req.params.event_id]);
+        const event = events[0]
+        const [organiser] = await connection.execute(organiserQuery)
+        const eventDateTime = event.date_time.toISOString().split('T')
+        event.date = eventDateTime[0]
+        event.time = eventDateTime[1].slice(0, -5);
+        console.log(event)
+        res.render("event/update", {
+            event, organiser
+        })
+    })
+
+    // DELETE event route
+    app.get("/event/:event_id/delete", async (req, res) => {
+        const sql = `SELECT * FROM event WHERE event_id = ?`;
+        const [events] = await connection.execute(sql, [req.params.event_id])
+        const event = events[0]
+        res.render("event/delete", {
+            event
+        })
+    })
+
+    app.post("/event/:event_id/delete", async (req, res) => {
+        const query = `DELETE FROM event WHERE event_id = ?`
+        await connection.execute(query, [req.params.event_id])
+        res.redirect('/event');
+    })
 
     // Homepage
     app.get('/', function (req, res) {
@@ -149,4 +211,17 @@ app.listen(3000, () => {
 //Functions to make SQL searching easier
 function findById(tableName, table_id) {
     return `SELECT * FROM ${tableName} WHERE ${table_id} = ?`
+}
+
+// Function to format date
+function formatDate(dateString) {
+    const date = new Date(dateString);
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+    const seconds = String(date.getSeconds()).padStart(2, '0');
+
+    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
 }
